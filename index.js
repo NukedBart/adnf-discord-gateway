173061e0f2ad7f598005da646bbe9420de54db13
NukedBart <oxbilibiliox@gmail.com>
2023-07-13 16:23:27 +0800

    registration implement

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+const http = require('follow-redirects').http;
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
@@ -144,25 +146,42 @@ const clickable_slash_commands = [
     }
   ];
 
+const sendPublic = (pending_content) => {
+  res.send({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+      content: pending_content
+    }
+  });
+}
+
+const sendPrivate = (pending_content) => {
+  res.send({
+    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+    data: {
+      content: pending_content,
+      flags: 64
+    }
+  });
+}
+
 const parseCommands = (commands, isAdmin=false, index = 0) => {
-  if (index >= commands.length) {
+  if (index >= commands.length) 
     return '';
-  }
+  
   const command = commands[index];
   const formattedCommand = `</${command.name}:${command.id}>  ${command.description}\n`;
   
-  if(command.options.isPublic) {
+  if(command.options.isPublic) 
     return formattedCommand + parseCommands(commands, isAdmin, index + 1);
-  }
-  if(isAdmin) {
+    
+  if(isAdmin) 
     return formattedCommand + parseCommands(commands, isAdmin, index + 1);
-  }
+    
   return parseCommands(commands, isAdmin, index + 1);
 }
 
-// handle /help
-const handleHelpCommand = async (req, res) => {
-  let isAdmin = false; // 默认为 false，表示不具有 "Staff" 角色
+const hasAdminRole = async (req, res) => {
   try {
     const guildId = req.body.guild.id; // get server ID
     const rolesResponse = await discordApi.get(`/guilds/${guildId}/roles`); // get server roles
@@ -170,43 +189,104 @@ const handleHelpCommand = async (req, res) => {
     // roles are now accessible
     console.log(roles);
 
-    isAdmin = req.body.member.roles.some(roleId => roles.find(role => role.id === roleId)?.name === 'Staff');
+    return req.body.member.roles.some(roleId => roles.find(role => role.id === roleId)?.name === 'Staff');
   } catch (error) {
     console.error('Error fetching guild roles:', error);
-    // error handler
+    return false;
   }
-  const formattedCommands = parseCommands(clickable_slash_commands, isAdmin);
-  console.log(req.body.member.user);
-  console.log(req.body.member);
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: formattedCommands,
-      flags: 64
+}
+
+const isAlphanumeric = (string) => {
+  const regex = /^[a-zA-Z0-9]{6,16}$/;
+  return regex.test(string);
+}
+
+const checkPasswordStrength = (string) => {
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,64}$/;
+  return regex.test(string);
+}
+
+const processRegister = (username, password, discordUsername, userId) => {
+  if (!isAlphanumeric(username)) return '4062';
+  if (!checkPasswordStrength(username)) return '4063';
+  const options = {
+    method: 'POST',
+    hostname: '43.248.97.194',
+    port: 60280,
+    path: '/api/register',
+    headers: {
+      'Content-Type': 'multipart/form-data; boundary=botRegisterRequest'
     }
+  };
+  
+  // Construct the request payload
+  const postData = `--botRegisterRequest\r\nContent-Disposition: form-data; name='username'\r\n\r\n${username}\r\n--botRegisterRequest\r\nContent-Disposition: form-data; name='password'\r\n\r\n${password}\r\n--botRegisterRequest\r\nContent-Disposition: form-data; name='discord'\r\n\r\n${discordUsername}\r\n--botRegisterRequest\r\nContent-Disposition: form-data; name='discordid'\r\n\r\n${userId}\r\n--botRegisterRequest--`;
+  
+  // Send the HTTP request
+  const httpRequest = http.request(options, function (response) {
+    const chunks = [];
+  
+    response.on('data', function (chunk) {
+      chunks.push(chunk);
+    });
+  
+    response.on('end', function (chunk) {
+      const body = Buffer.concat(chunks);
+      return body.toString();
+    });
+  
+    response.on('error', function (error) {
+      console.error(error);
+      return '5000';
+    });
   });
+}
+
+// handle /help
+const handleHelpCommand = async (req, res) => {
+  let isAdmin = await hasAdminRole(req, res);
+  sendPrivate(parseCommands(clickable_slash_commands, isAdmin));
 };
 
 // handle /register
 const handleRegisterCommand = (req, res) => {
-  // Access the value of 'username' parameter from the request body
+  // Access the value of parameters from the request body
   const username = req.body.data.options.find(option => option.name === 'username').value;
   const password = req.body.data.options.find(option => option.name === 'password').value;
   const discordUsername = req.body.member.user.username;
   const userId = req.body.member.user.id;
   const discriminator = req.body.member.user.discriminator;
   
-  // Use the 'username' value as needed
+  // Use the values as needed
   console.log('Username:', username);
-  console.log('Issuer:', discordUsername, "#", discriminator, " (", userId, ")");
+  console.log('Issuer:', discordUsername, '#', discriminator, ' (', userId, ')');
   
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: `${discordUsername} Register command received. Username: ${username}, Password: ||${password}||`,
-      flags: 64 // Set the Ephemeral flag to make the message visible only to the user
-    }
-  });
+  const result = processRegister(username, password, discordUsername + discriminator, userId);
+  
+  switch (result)
+  {
+  case '4061':
+    sendPrivate(`<@${userId}> That usename has already been taken. Try another one.`);
+    break;
+  case '4062':
+    sendPrivate(`<@${userId}> That usename is invalid. \nA valid username should only contain A-Z, a-z, 0-9; and should contain at least 6 characters; and 16 characters maximum. \nEx. Validname123`);
+    break
+  case '4063':
+    sendPrivate(`<@${userId}> The provided password is either invalid or not secure enough. \nYour password should be: \n -At least 8 characters long. \n -Contain an upper-case letter. \n -Contain a lower-case letter. \n -Contain a number. \n -64 characters maximum. \n Ex. Val1dpassw0rd`);
+    break;
+  case 'bruh':
+    sendPrivate(`<@${userId}> Sorry, but an unknown error had occurred.`);
+    break
+  case '5000':
+    sendPrivate(`<@${userId}> Sorry, but an unknown error had occurred.`);
+    break;
+  case '200':
+    sendPrivate(`<@${userId}> Your ADNF account ${username} with password ||${password}|| is now ready and linked to ${discordUsername}#${discriminator}<${userId}> !`);
+    break;
+  default:
+    sendPrivate(`<@${userId}> Sorry, but an unknown error had occurred.`);
+    break;
+  }
 };
 
 // handle /link
@@ -221,14 +301,10 @@ const handleLinkCommand = (req, res) => {
   // Use the 'username' value as needed
   console.log('Username:', username);
   console.log('Issuer:', discordUsername);
+  
+  // TODO: add link function
 
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: `${discordUsername} Link command received. Username: ${username}, Password: ||${password}||`,
-      flags: 64
-    }
-  });
+  sendPrivate(`${discordUsername} Link command received. Username: ${username}, Password: ||${password}||`);
 };
 
 // handle /relink
@@ -244,92 +320,51 @@ const handleRelinkCommand = (req, res) => {
   console.log('Username:', username);
   console.log('Issuer:', discordUsername);
 
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: `${discordUsername} Link command received. Username: ${username}, Password: ||${password}||`,
-      flags: 64
-    }
-  });
+  // TODO: add relink function
+
+  sendPrivate(`${discordUsername} Relink command received. Username: ${username}, Password: ||${password}||`);
 };
 
 // handle /mention
 const handleMentionCommand = async (req, res) => {
-  let isAdmin = false; // 默认为 false，表示不具有 "Staff" 角色
-  try {
-    const guildId = req.body.guild.id; // get server ID
-    const rolesResponse = await discordApi.get(`/guilds/${guildId}/roles`); // get server roles
-    const roles = rolesResponse.data; // get roles raw data
-    // roles are now accessible
-    console.log(roles);
-
-    isAdmin = req.body.member.roles.some(roleId => roles.find(role => role.id === roleId)?.name === 'Staff');
-  } catch (error) {
-    console.error('Error fetching guild roles:', error);
-    // error handler
-  }
+  let isAdmin = await hasAdminRole(req, res);
   const mentionedUserId = req.body.data.options.find(option => option.name === 'userid')?.value;
-  console.log(req.body.member.roles);
   if (!isAdmin) {
-    res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: 'You do not have permission to use this command.',
-        flags: 64
-      }
-    });
+    sendPrivate('You do not have permission to use this command.');
     return;
   }
-
   if (!mentionedUserId) {
-    res.send({
-      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-      data: {
-        content: 'Please provide a valid user ID.',
-        flags: 64
-      }
-    });
+    sendPrivate('Please provide a valid user ID.');
     return;
   }
-
-  res.send({
-    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-    data: {
-      content: `Hey <@${mentionedUserId}>! One of our admins had mentioned you! Is something bad happening?`
-    }
-  });
+  sendPublic(`Hey <@${mentionedUserId}>! One of our admins had mentioned you! Is something bad happening?`);
 };
 
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async (req, res) => {
   const interaction = req.body;
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = interaction.data;
+  if (interaction.type !== InteractionType.APPLICATION_COMMAND) return;
+  const { name } = interaction.data;
 
-    switch (name) {
-      case 'help':
-        handleHelpCommand(req, res);
-        break;
-      case 'register':
-        handleRegisterCommand(req, res);
-        break;
-      case 'link':
-        handleLinkCommand(req, res);
-        break;
-      case 'relink':
-        handleRelinkCommand(req, res);
-        break;
-      case 'mention':
-        handleMentionCommand(req, res);
-        break;
-      default:
-        res.send({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: 'Invalid command!'
-          }
-        });
-    }
+  switch (name) 
+  {
+  case 'help':
+    handleHelpCommand(req, res);
+    break;
+  case 'register':
+    handleRegisterCommand(req, res);
+    break;
+  case 'link':
+    handleLinkCommand(req, res);
+    break;
+  case 'relink':
+    handleRelinkCommand(req, res);
+    break;
+  case 'mention':
+    handleMentionCommand(req, res);
+    break;
+  default:
+    sendPublic'Invalid command!');
   }
 });
 
